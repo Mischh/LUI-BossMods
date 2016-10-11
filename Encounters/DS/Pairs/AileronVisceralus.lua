4da510e5968cc9ab1298201b5a7f197329e7804f
Mischh <mischhaffliction@gmail.com>
2016-10-11 14:13:02 +0200

    first Implementations of Water/Air, Life/Air, Earth/Logic - partially tested.

--- Encounters/DS/Pairs/AileronVisceralus.lua
+++ Encounters/DS/Pairs/AileronVisceralus.lua
@@ -1,94 +1,206 @@
 require "Window"
 require "Apollo"
 
-local Mod = {}
 local LUI_BossMods = Apollo.GetAddon("LUI_BossMods")
-local Encounter = "AileronVisceralus"
+local Mod = LUI_BossMods:EncounterPrototype("AileronVisceralus")
 
-local Locales = {
-    ["enUS"] = {
+Mod:Locales(
+    {--[[enUS]] 
         -- Unit names
         ["unit.boss_air"] = "Aileron",
         ["unit.boss_life"] = "Visceralus",
+		["unit.thorns"] = "Wild Brambles",
+		["unit.jumppad"] = "[DS] e395 - Air - Tornado",
+		["unit.healthorb"] = "Life Force",
+		["unit.tree"] = "Lifekeeper",
+		
+		["label.healthorb"] = "Health Orb",
+		["label.midphase"] = "Midphase",
+		["label.midphase_end"] = "Midphase Ending",
+		["label.thorns"] = "Thorns",
+		["label.twirl"] = "Twirl",
+		["label.trees"] = "Next Trees",
+		
+		--alerts
+		["alert.twirlyou"] = "Twirl on YOU!",
     },
-    ["deDE"] = {},
-    ["frFR"] = {},
-}
+    {--[[deDE]] 
+        -- Unit names
+        ["unit.boss_air"] = "Aileron",
+        ["unit.boss_life"] = "Viszeralus",
+		["unit.thorns"] = "Wilde Brombeeren",
+		["unit.jumppad"] = "[DS] e395 - Air - Tornado",
+		["unit.healthorb"] = "Lebenskraft",
+		["unit.tree"] = "Lebensbewahrer",
+		
+		["label.healthorb"] = "Lebenskugel",
+		["label.midphase"] = "Mittelphase",
+		["label.midphase_end"] = "Mittelphase Endet",
+		["label.thorns"] = "Dornen",
+		["label.twirl"] = "Wirbel",
+		["label.trees"] = "Nächste Bäume",
+		
+		--alerts
+		["alert.twirlyou"] = "Wirbel auf DIR!",
+	}, 
+    {--[[frFR]] 
+        -- Unit names
+        ["unit.boss_air"] = "Ventemort",
+        ["unit.boss_life"] = "Visceralus",
+		["unit.thorns"] = "Ronces sauvages",
+		["unit.jumppad"] = "[DS] e395 - Air - Tornado",
+		["unit.healthorb"] = "Force vitale",
+		["unit.tree"] = "Garde-vie",
+		
+		["label.healthorb"] = "Health Orb",
+		["label.midphase"] = "Midphase",
+		["label.midphase_end"] = "Midphase Ending",
+		["label.thorns"] = "Thorns",
+		["label.twirl"] = "Twirl",
+		["label.trees"] = "Next Trees",
+		
+		--alerts
+		["alert.twirlyou"] = "Twirl on YOU!",
+	}
+)
 
-function Mod:new(o)
-    o = o or {}
-    setmetatable(o, self)
-    self.__index = self
-    self.instance = "Datascape"
-    self.displayName = "Aileron & Visceralus"
-    self.groupName = "Elemental Pairs"
-    self.tTrigger = {
-        sType = "ALL",
-        tZones = {
-            [1] = {
-                continentId = 52,
-                parentZoneId = 98,
-                mapId = 119,
-            },
-        },
-        tNames = {
-            ["enUS"] = {"Aileron","Visceralus"},
-        },
-    }
-    self.run = false
-    self.runtime = {}
-    self.config = {
-        enable = true,
-        units = {
-            boss_air = {
-                enable = true,
-                label = "unit.boss_air",
-                color = "af00ffff",
-            },
-            boss_life = {
-                enable = true,
-                label = "unit.boss_life",
-                color = "af228b22",
-            },
-        },
-    }
-    return o
+function Mod:Setup()
+	name("Datascape", "Aileron & Visceralus", "Elemental Pairs")
+	trigger("ALL", {"Aileron","Visceralus"}, {"Aileron","Viszeralus"}, {"Ventemort","Visceralus"}, {continentId = 52, parentZoneId = 98, mapId = 119})
+	
+	unit("boss_air", true, 1, "af00ffff", "unit.boss_air")
+	unit("boss_life", true, 2, "af228b22", "unit.boss_life")
+	
+	line("line_healthorb", true, "ff0000ff", 7, "label.healthorb")
+	
+	timer("timer_midphase", true, "af00ffff", "label.midphase")
+	timer("timer_midphase_end", true, "afff0000", "label.midphase_end")
+	timer("timer_thorns", true, "af228b22", "label.thorns")
+	timer("timer_twirl", true, "afff0000", "label.twirl")
+	timer("timer_trees", true, "af7cfc00", "label.trees")
+	
+	alert("alert_twirl", true, nil, nil, "alert.twirlyou")
+	sound("sound_twirl", true, "inferno", "alert.twirlyou")
+	icon("icon_twirl", true, "target", 40, "ffff0000", "label.twirl")
 end
 
-function Mod:Init(parent)
-    Apollo.LinkAddon(parent, self)
+function Mod:SetupEvents()
+	local DEBUFFID_TWIRL = 70440
 
-    self.core = parent
-    self.L = parent:GetLocale(Encounter,Locales)
+	onUnitCreated("BossCreated_Air", self.L["unit.boss_air"], true)
+	onUnitCreated("BossCreated_Life", self.L["unit.boss_life"], true)
+	
+	onUnitCreated("ThornsSpawned", self.L["unit.thorns"])
+	
+	onUnitCreated("JumppadSpawned", self.L["unit.jumppad"])
+	onUnitCreated("JumppadDespawned", self.L["unit.jumppad"])
+	
+	onUnitCreated("HealthOrbSpawned", self.L["unit.healthorb"])
+	onUnitDestroyed("HealthOrbDespawned", self.L["unit.healthorb"])
+	
+	onUnitCreated("TreeSpawned", self.L["unit.tree"])
+	
+	onBuffAdded("TwirlAdded", nil, nil, DEBUFFID_TWIRL)
+	onBuffRemoved("TwirlRemoved", nil, nil, DEBUFFID_TWIRL)
 end
 
-function Mod:OnUnitCreated(nId, tUnit, sName, bInCombat)
-    if not self.run == true then
-        return
-    end
+local nLifeBossId = 0
+local nTreeKeeperCount = 0
+local tTreeKeeperList = {}
+local nFirstTreeId = 0
+local nLastThornsTime = 0
+local bIsMidPhase = false
+local nMidPhaseTime = 0
+local nTwirlCount = 0
 
-    if sName == self.L["unit.boss_air"] and bInCombat == true then
-        self.core:AddUnit(nId,sName,tUnit,self.config.units.boss_air.enable,false,false,false,nil,self.config.units.boss_air.color)
-    elseif sName == self.L["unit.boss_life"] and bInCombat == true then
-        self.core:AddUnit(nId,sName,tUnit,self.config.units.boss_life.enable,false,false,false,nil,self.config.units.boss_life.color)
-    end
+function Mod:OnStart()
+	nLastThornsTime = 0
+    bIsMidPhase = false
+    nMidPhaseTime = GameLib.GetGameTime() + 90
+    nTwirlCount = 0
+    nTreeKeeperCount = 0
+    tTreeKeeperList = {}
+    nFirstTreeId = nil
+	
+	self:AddTimer("timer_midphase", 90)
+	self:AddTimer("timer_thorns", 20)
+    --mod:AddTimerBar("AVATUS_INCOMING", "Avatus incoming", 500)
 end
 
-function Mod:IsRunning()
-    return self.run
+function Mod:OnEnd()
 end
 
-function Mod:IsEnabled()
-    return self.config.enable
+function Mod:BossCreated_Air(nId, tUnit, sName, bInCombat)
+	self:AddUnit("boss_air", tUnit, false, false, false)
 end
 
-function Mod:OnEnable()
-    self.run = true
+function Mod:BossCreated_Life(nId, tUnit, sName, bInCombat)
+	nLifeBossId = nId
+	self:AddUnit("boss_life", tUnit, false, false, false)
 end
 
-function Mod:OnDisable()
-    self.run = false
+function Mod:ThornsSpawned(nId, tUnit, sName, bInCombat)
+	local nCurrentTime = GameLib.GetGameTime()
+	if nLastThornsTime + 5 < nCurrentTime and nCurrentTime + 16 < nMidPhaseTime then
+		nLastThornsTime = nCurrentTime
+		nTwirlCount = nTwirlCount + 1
+		self:AddTimer("timer_thorns", 15)
+		if nTwirlCount % 2 == 1 then
+			self:AddTimer("timer_twirl", 15)
+		end
+	end
 end
 
-local ModInst = Mod:new()
-LUI_BossMods.modules[Encounter] = ModInst
+function Mod:JumppadSpawned(nId, tUnit, sName, bInCombat)
+	if not bIsMidPhase then
+		local nCurrentTime = GameLib.GetGameTime()
+		bIsMidPhase = true
+		nTwirlCount = 0
+		nMidPhaseTime = nCurrentTime + 115
+		self:AddTimer("timer_midphase_end", 35)
+		self:AddTimer("timer_thorns", 35)
+		self:AddTimer("timer_trees", 35)
+	end
+end
+
+function Mod:JumppadDespawned(nId, tUnit, sName, bInCombat)
+    if bIsMidPhase then
+        bIsMidPhase = false
+		self:AddTimer("timer_midphase", 90)
+	end
+end
+
+function Mod:HealthOrbSpawned(nId, tUnit, sName, bInCombat)
+	self:DrawLineBetween("line_healthorb", tUnit, nLifeBossId, nil, nil, "line_healthorb"..nId)
+end
+
+function Mod:HealthOrbDespawned(nId, tUnit, sName, bInCombat)
+	self:RemoveLineBetween("line_healthorb"..nId)
+end
+
+function Mod:TreeSpawned(nId, tUnit, sName, bInCombat)
+	nTreeKeeperCount = nTreeKeeperCount + 1
+	if nTreeKeeperCount % 2 == 0 then
+		if (nTreeKeeperCount + 2) % 4 == 0 then -- nTreeKeeperCount == 2, 6, 10, 14, ...
+			self:AddTimer("timer_trees", 30)
+		end
+	end
+end
+
+function Mod:TwirlAdded(nId, nSpellId, sName, tData, sUnitName, nStack, nDuration) 
+	local tUnit = tData.tUnit
+	if tUnit:IsThePlayer() then
+		self:ShowAlert("alert_twirl")
+		self:PlaySound("sound_twirl")
+	else
+		self:DrawIcon("icon_twirl", tUnit, 30, 20, nil, "twirl"..tUnit:GetId()) --show for max 20s
+	end
+end
+
+function Mod:TwirlRemoved(nId, nSpellId, sName, tData, sUnitName)
+	if not tData.tUnit:IsThePlayer() then
+		self:RemoveIcon("twirl"..tData.tUnit:GetId())
+	end
+end
+
+Mod:new()
--- Encounters/DS/Pairs/MnemesisMegalith.lua
+++ Encounters/DS/Pairs/MnemesisMegalith.lua
@@ -1,94 +1,283 @@
 require "Window"
 require "Apollo"
 
-local Mod = {}
 local LUI_BossMods = Apollo.GetAddon("LUI_BossMods")
-local Encounter = "MnemesisMegalith"
+local Mod = LUI_BossMods:EncounterPrototype("MnemesisMegalith")
 
-local Locales = {
-    ["enUS"] = {
+local DEBUFF_SNAKE = 74570
+local snakeStepLength = 6 --the Snake does a Step of 6m each time.
+Mod:Locales(
+    {--[[enUS]] 
         -- Unit names
         ["unit.boss_logic"] = "Mnemesis",
         ["unit.boss_earth"] = "Megalith",
+		["unit.snake"] = "e395- [Datascape] Logic Elemental - Snake Piece (invis unit)",
+		["unit.stone"] = "Obsidian Outcropping",
+		["unit.pillar"] = "Crystalline Matrix",
+		
+		-- Casts
+		["cast.defragment"] = "Defragment",
+		
+		-- Labels
+		["label.snakemap"] = "Snake Map",
+		["label.youtarget"] = "You: Targeted by Snake",
+		["label.target"] = "Targeted by Snake",
+		["label.retarget"] = "Snake: New Target",
+		["label.nextdefragment"] = "Next defragment",
     },
-    ["deDE"] = {},
-    ["frFR"] = {},
-}
+    {--[[deDE]] 
+		-- Unit names
+        ["unit.boss_logic"] = "Mnemesis",
+        ["unit.boss_earth"] = "Megalith",
+		["unit.snake"] = "e395- [Datascape] Logic Elemental - Snake Piece (invis unit)",
+		["unit.stone"] = "Obsidianvorsprung",
+		["unit.pillar"] = "Kristallmatrix",
+		
+		-- Casts
+		["cast.defragment"] = "Defragmentieren",
+		
+		["label.snakemap"] = "Schlangen Karte",
+		["label.youtarget"] = "Du: Von Schlange verfolgt",
+		["label.target"] = "Von Schlange verfolgt",
+		["label.retarget"] = "Schlange: Neues Ziel",
+		["label.nextdefragment"] = "Nächstes Defragmentieren",
+	}, 
+    {--[[frFR]] 
+		-- Unit names
+        ["unit.boss_logic"] = "Mnemesis",
+        ["unit.boss_earth"] = "Megalith",
+		["unit.snake"] = "e395- [Datascape] Logic Elemental - Snake Piece (invis unit)",
+		["unit.stone"] = "Affleurement d'obsidienne",
+		["unit.pillar"] = "Matrice cristalline",
+		
+		-- Casts
+		["cast.defragment"] = "Défragmentation",
+		
+		-- Labels
+		["label.snakemap"] = "Snake Map",
+		["label.youtarget"] = "You: Targeted by Snake",
+		["label.target"] = "Targeted by Snake",
+		["label.retarget"] = "Snake: New Target",
+		["label.nextdefragment"] = "Prochaine defragmentation",
+	}
+)
 
-function Mod:new(o)
-    o = o or {}
-    setmetatable(o, self)
-    self.__index = self
-    self.instance = "Datascape"
-    self.displayName = "Mnemesis & Megalith"
-    self.groupName = "Elemental Pairs"
-    self.tTrigger = {
-        sType = "ALL",
-        tZones = {
-            [1] = {
-                continentId = 52,
-                parentZoneId = 98,
-                mapId = 117,
-            },
-        },
-        tNames = {
-            ["enUS"] = {"Mnemesis","Megalith"},
-        },
-    }
-    self.run = false
-    self.runtime = {}
-    self.config = {
-        enable = true,
-        units = {
-            boss_logic = {
-                enable = true,
-                label = "unit.boss_logic",
-                color = "afadff2f",
-            },
-            boss_earth = {
-                enable = true,
-                label = "unit.boss_earth",
-                color = "afff932f",
-            },
-        },
-    }
-    return o
+function Mod:Setup()
+	name("Datascape", "Mnemesis & Megalith", "Elemental Pairs")
+	trigger("ALL", {"Mnemesis","Megalith"}, {"Mnemesis","Megalith"}, {"Mnemesis","Megalith"}, {continentId = 52, parentZoneId = 98, mapId = 117})
+	
+	unit("boss_logic", true, 1, "afadff2f", "unit.boss_logic")
+	unit("boss_earth", true, 2, "afff932f", "unit.boss_earth")
+	unit("unit_pillar", true, 3, "afff0000", "unit.pillar")
+	
+	--snake-stuff
+	line("line_snakemap", true, "ffff4500", 26, "label.snakemap")
+	aura("aura_youtarget", true, "run", "afadff2f", "label.youtarget")
+	icon("icon_target", true, "target", 50, "afadff2f", "label.target")
+	timer("timer_retarget", true, "afadff2f", "label.retarget")
+	
+	--defragment
+	timer("timer_defrag", true, "afff0000", "label.nextdefragment")
+	cast("cast_defrag", true, "afff0000", "cast.defragment")
 end
 
-function Mod:Init(parent)
-    Apollo.LinkAddon(parent, self)
-
-    self.core = parent
-    self.L = parent:GetLocale(Encounter,Locales)
+function Mod:SetupEvents()
+	onUnitCreated("BossCreated_Logic", self.L["unit.boss_logic"], true)
+	onUnitCreated("BossCreated_Earth", self.L["unit.boss_earth"], true)
+	
+	onUnitCreated("PillarCreated", self.L["unit.pillar"])
+	onUnitDestroyed("PillarRemoved", self.L["unit.pillar"])
+	
+	--snake stuff
+	onUnitCreated("StoneCreated", self.L["unit.stone"])
+	onUnitDestroyed("StoneDestroyed", self.L["unit.stone"])
+	onUnitCreated("SnakePieceCreated", self.L["unit.snake"])
+	onBuffAdded("SnakeBuffApplied", nil, nil, DEBUFF_SNAKE)
+	
+	--defrag
+	onCastStart("CastStart_Defrag", "boss_logic", nil, self.L["cast.defragment"])
 end
 
-function Mod:OnUnitCreated(nId, tUnit, sName, bInCombat)
-    if not self.run == true then
-        return
-    end
-
-    if sName == self.L["unit.boss_logic"] and bInCombat == true then
-        self.core:AddUnit(nId,sName,tUnit,self.config.units.boss_logic.enable,false,false,false,nil,self.config.units.boss_logic.color)
-    elseif sName == self.L["unit.boss_earth"] and bInCombat == true then
-        self.core:AddUnit(nId,sName,tUnit,self.config.units.boss_earth.enable,false,false,false,nil,self.config.units.boss_earth.color)
-    end
+local tStones = {}
+local closestStone = nil
+local snakePos = nil
+local targetUnit = nil
+local numSnakeLines = 0
+function Mod:OnStart()
+	tStones = {}
+	closestStone = nil
+	snakeUnit = nil
+	targetUnit = nil
+	numSnakeLines = 0
 end
 
-function Mod:IsRunning()
-    return self.run
+local abs = math.abs
+local floor = math.floor
+local ceil = math.ceil
+function Mod:GetSnakeMovement(tUnit)
+	if snakePos and tUnit then
+		local info = {}
+		local pos = tUnit:GetPosition()
+		local x = pos.x-snakePos.x
+		local y = pos.z-snakePos.z
+		
+		local xdir = x<0 and -1 or 1
+		local ydir = y<0 and -1 or 1
+		
+		local xnum = ceil(floor(abs(x)/snakeStepLength*2)/2)-- the number of Steps into X direction
+		local ynum = ceil(floor(abs(y)/snakeStepLength*2)/2)
+		
+		local prioX = abs(x)+((ynum-xnum)*snakeStepLength) > abs(y)
+		
+		
+		if xnum > ynum then
+			local diff = xnum-ynum
+			
+			info.straight = {
+				x = snakePos.x+xdir*diff*snakeStepLength,
+				y = snakePos.y,
+				z = snakePos.z,
+			}
+			info.diagonalSteps = ynum
+			info.diagonalDirection = {x=xdir, z=ydir}
+			info.priorizeX = prioX
+		else
+			local diff = ynum-xnum
+		
+			info.straight = {
+				x = snakePos.x,
+				y = snakePos.y,
+				z = snakePos.z+ydir*diff*snakeStepLength,
+			}
+			info.diagonalSteps = xnum
+			info.diagonalDirection = {x=xdir, z=ydir}
+			info.priorizeX = prioX
+		end
+		return info
+	else
+		return nil
+	end
 end
 
-function Mod:IsEnabled()
-    return self.config.enable
+function Mod:UpdateSnakeLines(force) --this gets forced, whenever something 'big' happens (new Stone, new Target, new SnakeBlock)
+	if not closestStone or force then
+		local info = self:GetSnakeMovement(closestStone or targetUnit)
+		
+		if not info then return end
+		
+		self:DrawLineBetween("line_snakemap", snakePos, info.straight, nil, nil, "SnakeLine"..(0))
+		
+		local xOff = info.diagonalDirection.x*snakeStepLength
+		local zOff = info.diagonalDirection.z*snakeStepLength
+		
+		local pointA, pointB = info.straight, nil
+		if info.priorizeX then--draw the X-Direction first. (Of the diagonal Part)
+			for i = 1, info.diagonalSteps, 1 do
+				pointB = { --Create new Tables! Important!
+					x = pointA.x + xOff, 
+					y = pointA.y, 
+					z = pointA.z,
+				}
+				self:DrawLineBetween("line_snakemap", pointA, pointB, nil, nil, "SnakeLine"..(2*i-1))
+				pointA = { --Create new Tables! Important!
+					x = pointB.x, 
+					y = pointB.y, 
+					z = pointB.z + zOff,
+				}
+				self:DrawLineBetween("line_snakemap", pointB, pointA, nil, nil, "SnakeLine"..(2*i))
+			end
+		else
+			for i = 1, info.diagonalSteps, 1 do
+				pointB = { --Create new Tables! Important!
+					x = pointA.x, 
+					y = pointA.y, 
+					z = pointA.z + zOff,
+				}
+				self:DrawLineBetween("line_snakemap", pointA, pointB, nil, nil, "SnakeLine"..(2*i-1))
+				pointA = { --Create new Tables! Important!
+					x = pointB.x + xOff, 
+					y = pointB.y, 
+					z = pointB.z,
+				}
+				self:DrawLineBetween("line_snakemap", pointB, pointA, nil, nil, "SnakeLine"..(2*i))
+			end
+		end
+		
+		for i = 2*info.diagonalSteps+1, numSnakeLines, 1 do
+			self:RemoveLineBetween("SnakeLine"..i)
+		end
+		numSnakeLines = 2*info.diagonalSteps
+	end
 end
 
-function Mod:OnEnable()
-    self.run = true
+function Mod:BossCreated_Logic(nId, tUnit, sName, bInCombat)
+	self:AddUnit("boss_logic", tUnit, true, false, false) --oncast, onbuff, ondebuff
 end
 
-function Mod:OnDisable()
-    self.run = false
+function Mod:BossCreated_Earth(nId, tUnit, sName, bInCombat)
+	self:AddUnit("boss_earth", tUnit, false, false, false)
 end
 
-local ModInst = Mod:new()
-LUI_BossMods.modules[Encounter] = ModInst
+function Mod:PillarCreated(nId, tUnit, sName, bInCombat)
+	self:AddUnit("unit_pillar", tUnit, false, false, false, nil, "pillar"..nId)
+end
+
+function Mod:PillarRemoved(nId, tUnit, sName)
+	self:RemoveUnit("pillar"..nId)
+end
+
+function Mod:StoneCreated(nId, tUnit, sName, bInCombat)
+	if closestStone then
+		if self:GetDistance(snakePos, tUnit) < self:GetDistance(snakePos, closestStone) then
+			tStones[closestStone] = true
+			closestStone = tUnit
+		else
+			tStones[tUnit] = true
+			closestStone = closestStone
+		end
+	else
+		closestStone = tUnit
+	end
+	self:UpdateSnakeLines(true)
+end
+
+function Mod:StoneDestroyed(nId, tUnit, sName)
+	if tUnit == closestStone then
+		closestStone = nil
+	end
+end
+
+function Mod:SnakePieceCreated(nId, tUnit, sName, bInCombat)
+	snakePos = tUnit:GetPosition()
+	self:UpdateSnakeLines(true)
+end
+
+function Mod:SnakeBuffTimeout()
+	targetUnit = nil --the snake seems to bug out very often and doesnt apply any debuffs anymore, but still changes targets.
+	--from then on, we have no idea anymore, who is targeted - so we cant show.
+end
+
+function Mod:SnakeBuffApplied(nId, nSpellId, sName, tData, sUnitName, nStack, nDuration)
+	local tUnit = tData.tUnit	
+	
+	self:HideAura("aura_youtarget", false) --dont trigger SnakeBuffTimeout
+	self:HideIcon("icon_target", false)
+	
+	if tUnit:IsThePlayer() then
+		self:ShowAura("aura_youtarget", nDuration, true, nil, nil, nil, self.SnakeBuffTimeout) --after nDuration, call SnakeBuffTimeout
+	else
+		self:DrawIcon("icon_target", tUnit, 40, nDuration, true, nil, nil, nil, nil, self.SnakeBuffTimeout)
+	end
+	
+	self:AddTimer("timer_retarget", nDuration)
+	
+	targetUnit = tUnit
+	self:UpdateSnakeLines(true)
+end
+
+function Mod:CastStrart_Defrag(nId, sCastName, tCast, sName, nDuration)
+	self:ShowCast("cast_defrag", tCast)
+	self:AddTimer("timer_defrag", 40)
+end
+
+Mod:new()
